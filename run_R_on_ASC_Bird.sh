7afd9ac922876cf3253003ab101f1f3da21360f3
Gwendolyn Bird <36014677+birdoptera@users.noreply.github.com>
2019-02-04 09:02:59 -0600

    Add files via upload

---
+++ run_R_on_ASC_Bird.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+#  script to run R program
+# What I want to know about this is how it deals with packages. Do you have to install packages in your script, and what happens if they fail to install properly? 
+# load the paths and MPI
+source /opt/asn/etc/asn-bash-profiles-special/modules.sh
+module load R/3.5.1
+#
+# command to run R
+R CMD BATCH r_program.r
